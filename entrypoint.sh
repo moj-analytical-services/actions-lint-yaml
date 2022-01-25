b2fdc139f54f1ba673658ec288ab316d5af4a48e
Mike Ratford <michael.ratford@digital.justice.gov.uk>
2022-01-25 17:06:37 +0000

    correct bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-yamllint . -d "{extends: default, rules: {line-length:"\
-"{allow-non-breakable-words: true, allow-non-breakable-inline-mappings:"\
-"true}}}" --no-warnings
+yamllint . -d "{extends: default, rules: {line-length:
+{allow-non-breakable-words: true, allow-non-breakable-inline-mappings:
+true}}}" --no-warnings
